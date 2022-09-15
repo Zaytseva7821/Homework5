f4235600cf6d6cfb33a3f7d5cdb46bd6e8bf042d
Anastasia <zaytseva7821@ya.ru>
2022-09-15 20:33:02 +0400

    problem 2

---
+++ Problem 1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// // [345, 897, 568, 234] -> 2
+void FillArray(int[] array)
+{
+    for(int i=0; i< array.Length; i++)
+        array[i] = new Random().Next(100,999);
+}
+
+System.Console.WriteLine("Какой длины будет ваш массив?");
+int lenght = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [lenght];
+FillArray(array);
+System.Console.WriteLine("[{0}]",string.Join(", ", array));
+int count = 0;
+for(int i=0; i< array.Length; i++)
+{
+    if (array[i]%2 == 0)
+    count++;
+}
+System.Console.WriteLine($"Количество четных чисел в вашем массиве: {count}");
---
+++ Problem 2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+/// Я поняла это как "с нечетным индексом"
+void FillArray(int[] array)
+{
+    for(int i=0; i< array.Length; i++)
+        array[i] = new Random().Next(100,999);
+}
+
+System.Console.WriteLine("Какой длины будет ваш массив?");
+int lenght = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [lenght];
+FillArray(array);
+System.Console.WriteLine("[{0}]",string.Join(", ", array));
+int Sum = 0;
+for(int i=1; i< array.Length; i=i+2)
+{
+    Sum += array[i];
+}
+System.Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях {Sum}");
